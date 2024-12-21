d5bb454def9cb041cbdaacc7a118f0ccbe7772ce
640710505 <buangam_c2@silpakorn.edu>
2024-12-21 16:02:41 +0700

    change code

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Easy Example'),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-        body: Center(
-          child: MyCustomWidget(),
-        ),
-      ),
-    );
-  }
-}
-
-class MyCustomWidget extends StatefulWidget {
-  @override
-  _MyCustomWidgetState createState() => _MyCustomWidgetState();
-}
-
-class _MyCustomWidgetState extends State<MyCustomWidget> {
-  String _displayText = 'Hello, Flutter!';
-
-  void _changeText() {
-    setState(() {
-      _displayText = _displayText == 'Hello, Flutter!'
-          ? 'success test click_ver2'
-          : 'Hello, Flutter!';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          _displayText,
-          style: TextStyle(fontSize: 24),
-        ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: _changeText,
-          child: Text('Click Me'),
-        ),
-      ],
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text("Hello dara" , style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.yellow,
+                ),
+              
+              ),
+              backgroundColor: Colors.green,
+              
+              
+            ),
+            body:Container(
+              height:200,
+              width:500,
+              color : Colors.green,
+              padding: const EdgeInsets.all(60),
+              margin: const EdgeInsets.all(20),
+              child: const Text(
+                "paste your picture here",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ), 
+            ));
   }
 }
